aa40e30b4df9bd418fb4454418f86e8934bfd783
Lisa Martin <lejmartin@hotmail.com>
2018-01-19 16:29:11 -0500

    Commit first JS

---
+++ test.js
@@ -0,0 +1,9 @@
+
+//single line comments require the double slash
+/* multi line comments may be in block of code or instructions
+that span multiple lines */
+// console.log("something funny");
+//compute the result of 789/12345 and store it in a variable
+// console.log that variable
+var calc = 789 / 12345;
+console.log(calc);
